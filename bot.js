b783dceb28312cd6564be3e7f7cd49d416f4f1c3
shadamer <46399783+shadamer@users.noreply.github.com>
2019-01-20 22:54:43 -0600

    Add files via upload

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', message => {
 	
 	if (message.content === 'guild'){
 		var server = bot.guilds.get(message.guild.id).id;
-		message.reply(server);
+		message.reply('Guild:' + server);
 	}
 
 });
